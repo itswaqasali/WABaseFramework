d31d3ac37aed2e854e692c31a9cbfb978c51ff2a
waqasali <waqasali@axact.com>
2019-04-17 13:51:51 +0500

    initial commit

---
+++ WABaseFramework/Utility/UtilityClass.swift
@@ -0,0 +1,21 @@
+//
+//  UtilityClass.swift
+//  WABaseFramework
+//
+//  Created by Waqas Ali on 17/04/2019.
+//  Copyright © 2019 Ingic. All rights reserved.
+//
+
+import UIKit
+
+public class UtilityClass{
+    
+    public init(){
+        
+    }
+    
+    
+    public func printMe(){
+    print("Inside Utility Class")
+    }
+}
---
+++ WABaseFramework/WABaseFramework.h
@@ -0,0 +1,19 @@
+//
+//  WABaseFramework.h
+//  WABaseFramework
+//
+//  Created by Waqas Ali on 17/04/2019.
+//  Copyright © 2019 Ingic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WABaseFramework.
+FOUNDATION_EXPORT double WABaseFrameworkVersionNumber;
+
+//! Project version string for WABaseFramework.
+FOUNDATION_EXPORT const unsigned char WABaseFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WABaseFramework/PublicHeader.h>
+
+
